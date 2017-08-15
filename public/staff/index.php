66151c39425072f3fec34d422b580cd513f30676
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-15 20:01:50 +0300

    add basic template and styles for staff page

--- public/staff/index.php
+++ public/staff/index.php
@@ -2,11 +2,24 @@
 
 <html lang="en">
   <head>
-    <title></title>
+    <title>GBI</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
   </head>
 
   <body>
+    <header>
+      <h1>GBI Staff Area</h1>
+    </header>
 
+    <nav>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </nav>
+    <div id="content"></div>
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
   </body>
 </html>
