66560aadfbe2c602b34c7e2756fe4db940a282d5
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-21 20:35:03 +0300

    learn encode url parameters

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,3 +1,5 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
 
 $id = $_GET['id'] ?? '1'; //PHP > 7.0
@@ -5,3 +7,7 @@ $id = $_GET['id'] ?? '1'; //PHP > 7.0
 echo $id;
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
