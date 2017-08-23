16f046f81d6956b392f4294660165b9954e4c2f1
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-23 20:15:29 +0300

    add page redirection

--- private/functions.php
+++ private/functions.php
@@ -29,4 +29,10 @@ function error_500(){
     header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
     exit();
 }
+
+function redirect_to($location) {
+    header("Location: " . $location);
+    exit;
+}
+
 ?>
