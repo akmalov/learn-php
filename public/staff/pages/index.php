c9140f9f321cebf47b2c88f0b3f81425282a5678
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-28 21:47:05 +0300

    configure forms

--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -12,40 +12,42 @@ $pages = [
 <?php $page_title = 'Pages'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-  <div id="content">
-    <div class="pages listing">
-      <h1>Pages</h1>
+<div id="content">
+  <div class="pages listing">
+    <h1>Pages</h1>
 
-      <div class="actions">
-        <a class="action" href="">Create New Page</a>
-      </div>
-
-      <table class="list">
-        <tr>
-          <th>ID</th>
-          <th>Position</th>
-          <th>Visible</th>
-          <th>Name</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr>
-
-          <?php foreach($pages as $page) { ?>
-            <tr>
-              <td><?php echo h($page['id']); ?></td>
-              <td><?php echo h($page['position']); ?></td>
-              <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
-              <td><?php echo h($page['menu_name']); ?></td>
-              <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
-              <td><a class="action" href="">Edit</a></td>
-              <td><a class="action" href="">Delete</a></td>
-            </tr>
-          <?php } ?>
-      </table>
-
+    <div class="actions">
+      <a class="action" href="<?php echo url_for('/staff/pages/new.php'); ?>">Create New Page</a>
     </div>
 
+    <table class="list">
+      <tr>
+        <th>ID</th>
+        <th>Position</th>
+        <th>Visible</th>
+        <th>Name</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+      </tr>
+
+        <?php foreach ($pages as $page) { ?>
+          <tr>
+            <td><?php echo h($page['id']); ?></td>
+            <td><?php echo h($page['position']); ?></td>
+            <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
+            <td><?php echo h($page['menu_name']); ?></td>
+            <td><a class="action"
+                   href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
+            <td><a class="action"
+                   href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
+            <td><a class="action" href="">Delete</a></td>
+          </tr>
+        <?php } ?>
+    </table>
+
   </div>
+
+</div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -1,15 +1,16 @@
 <?php
+
 require_once('../../../private/initialize.php');
 
-if(!isset($_GET['id'])){
-  redirect_to(url_for('/staff/subjects/new.php'));
+if (!isset($_GET['id'])) {
+    redirect_to(url_for('/staff/subjects/index.php'));
 }
 $id = $_GET['id'];
 $menu_name = '';
 $position = '';
 $visible = '';
 
-if(is_post_request()) {
+if (is_post_request()) {
 
     // Handle form values sent by new.php
 
@@ -28,40 +29,45 @@ if(is_post_request()) {
 <?php $page_title = 'Edit Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-    <div id="content">
+  <div id="content">
 
-        <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+    <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
 
-        <div class="subject edit">
-            <h1>Edit Subject</h1>
+    <div class="subject edit">
+      <h1>Edit Subject</h1>
 
-            <form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
-                <dl>
-                    <dt>Menu Name</dt>
-                    <dd><input type="text" name="menu_name" value="<?php echo $menu_name; ?>" /></dd>
-                </dl>
-                <dl>
-                    <dt>Position</dt>
-                    <dd>
-                        <select name="position">
-                            <option value="1">1</option>
-                        </select>
-                    </dd>
-                </dl>
-                <dl>
-                    <dt>Visible</dt>
-                    <dd>
-                        <input type="hidden" name="visible" value="0" />
-                        <input type="checkbox" name="visible" value="1" />
-                    </dd>
-                </dl>
-                <div id="operations">
-                    <input type="submit" value="Edit Subject" />
-                </div>
-            </form>
-
+      <form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
+        <dl>
+          <dt>Menu Name</dt>
+          <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>"/></dd>
+        </dl>
+        <dl>
+          <dt>Position</dt>
+          <dd>
+            <select name="position">
+              <option value="1"<?php if ($position == "1") {
+                  echo " selected";
+              } ?>>1
+              </option>
+            </select>
+          </dd>
+        </dl>
+        <dl>
+          <dt>Visible</dt>
+          <dd>
+            <input type="hidden" name="visible" value="0"/>
+            <input type="checkbox" name="visible" value="1"<?php if ($visible == "1") {
+                echo " checked";
+            } ?> />
+          </dd>
+        </dl>
+        <div id="operations">
+          <input type="submit" value="Edit Subject"/>
         </div>
+      </form>
 
     </div>
+
+  </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
